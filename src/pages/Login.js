a813d69e446b24133ec66317ce2e25724dcebd9b
shubham-hmh <shubham_203054@saitm.org>
2024-07-15 14:34:29 +0530

    fail

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -26,7 +26,7 @@ const Login = () => {
     },
     validationSchema: loginSchema,
     onSubmit: values => {
-      dispatch(loginUser(values));
+      // dispatch(loginUser(values));
     },
   });
 
